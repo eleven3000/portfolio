2f0f6887d26ce99d42be13679676f26e091a838b
Jean-Luc Herbst <eleven300011@gmail.com>
2022-03-08 22:07:23 +0100

    animation

--- lib/background/background.dart
+++ lib/background/background.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/background/bezier_clippers.dart';
 
-class MyBackground extends StatelessWidget {
+class MyBackground extends StatefulWidget {
   final Widget? child;
   const MyBackground({Key? key, this.child}) : super(key: key);
+
+  @override
+  State<MyBackground> createState() => _MyBackgroundState();
+}
+
+class _MyBackgroundState extends State<MyBackground> {
+  bool _selected = false;
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 200), () {
+      setState(() {
+        _selected = true;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +28,25 @@ class MyBackground extends StatelessWidget {
       children: [
         Stack(children: [
           Container(color: Theme.of(context).backgroundColor),
+          AnimatedPadding(
+            padding: EdgeInsets.only(top: _selected ? 25 : 1),
+            curve: Curves.easeOutQuad,
+            duration: const Duration(seconds: 1),
+            child: ClipPath(
+              clipper: CustomBezierClipper(),
+              child: Container(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
           ClipPath(
             clipper: CustomBezierClipper(),
             child: Container(
-              color: Theme.of(context).primaryColor,
+              color: Theme.of(context).backgroundColor,
             ),
           ),
         ]),
-        child ?? Container(),
+        widget.child ?? Container(),
       ],
     );
   }
